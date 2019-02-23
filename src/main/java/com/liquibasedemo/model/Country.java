90a2e32c7fa63a11978d4ea2cde1a15eeb4fb599
Pavan Jadda <jpavanaryan@gmail.com>
2019-02-22 22:45:20 -0500

    Liquibase bug fixed

--- src/main/java/com/liquibasedemo/model/Country.java
+++ src/main/java/com/liquibasedemo/model/Country.java
@@ -2,7 +2,6 @@ package com.liquibasedemo.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 
@@ -36,29 +35,4 @@ public class Country
     {
     }
 
-    public Country(String name)
-    {
-        this.name = name;
-    }
-
-    public Country(@Length(max = 100, min = 2) String name, Region region)
-    {
-        this.name = name;
-        this.region = region;
-    }
-
-    public Country(@Length(max = 200, min = 2) String name, @Length(max = 200, min = 2) String code, @Length(max = 2, min = 2) String isoCode, Region region)
-    {
-        this.name = name;
-        this.code = code;
-        this.isoCode = isoCode;
-        this.region = region;
-    }
-
-    public Country(@Length(max = 200, min = 2) String name, @Length(max = 200, min = 2) String code, @Length(max = 2, min = 2) String isoCode)
-    {
-        this.name = name;
-        this.code = code;
-        this.isoCode = isoCode;
-    }
 }
