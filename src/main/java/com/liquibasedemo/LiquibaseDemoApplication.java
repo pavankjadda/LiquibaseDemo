3b2d281b22b972d78ce6e9f27a212300c7b6a850
Pavan Jadda <jpavanaryan@gmail.com>
2020-02-25 20:38:52 -0500

    Rename packages

--- src/main/java/com/liquibasedemo/LiquibaseDemoApplication.java
+++ src/main/java/com/liquibasedemo/LiquibaseDemoApplication.java
@@ -6,11 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LiquibaseDemoApplication
 {
-
     public static void main(String[] args)
     {
         SpringApplication.run(LiquibaseDemoApplication.class, args);
     }
-
 }
 
